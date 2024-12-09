97bb12d87baddcb62cfe9109bc830662f4a01805
lakshay singhal <91775547+plakshay@users.noreply.github.com>
2024-12-10 00:16:46 +0530

    ui for sign up page completed

--- lib/feature/auth/presentation/pages/sign_up.dart
+++ lib/feature/auth/presentation/pages/sign_up.dart
@@ -11,57 +11,88 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  // the sign page has been created with the form but the text inside each textformfield cannot be retrieved, therefore we use controllers, 3 controllers needed(one for each field)
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController(); 
+  final nameController = TextEditingController(); //when the textediting controller is made, it is needed to be disposed off 
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose(); // super dispose - flutter disposes rest of the remaining stuff
+  }
+
+  // these email controllers are to be attached to the authfield
+
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Sign Up.",
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold
-            ),),
-            const SizedBox(
-              height: 30,
-            ),
-            const AuthField(hintText: 'Name'),
-            const SizedBox(
-              height: 15,
-            ),
-            const AuthField(hintText: 'Email'),
-            const SizedBox(
-              height: 15,
-            ),
-            const AuthField(hintText: 'Password'),
-            const SizedBox(
-              height: 20,
-            ),
-            const AuthGradientButton(),
-            const SizedBox(
-              height: 15,
-            ),
-            RichText( // rich text allopws to write 2 texts in same line
-              text: TextSpan(
-              text: 'Don\'t have an account? ',
-              style: Theme.of(context).textTheme.titleMedium,
-              children: [
-                TextSpan(
-                  text: 'Sign in',
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    color: AppPallete.gradient2,
-                    fontWeight: FontWeight.bold
-                  ),
-                  
-                )
-              ]
-            ),
-            ),
-            
-          ],
+        child: Form(
+          key: formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Sign Up.",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold
+              ),),
+              const SizedBox(
+                height: 30,
+              ),
+               AuthField(
+                hintText: 'Name', 
+                controller: nameController, 
+                ),
+              const SizedBox(
+                height: 15,
+              ),
+              AuthField(
+                hintText: 'Email', 
+                controller: emailController,
+                ),
+              const SizedBox(
+                height: 15,
+              ),
+               AuthField(
+                hintText: 'Password', 
+                controller: passwordController, 
+                isobsecureText: true,
+                ),
+              const SizedBox(
+                height: 20,
+              ),
+              const AuthGradientButton(),
+              const SizedBox(
+                height: 15,
+              ),
+              RichText( // rich text allopws to write 2 texts in same line
+                text: TextSpan(
+                text: 'Already have an account? ',
+                style: Theme.of(context).textTheme.titleMedium,
+                children: [
+                  TextSpan(
+                    text: 'Sign in',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: AppPallete.gradient2,
+                      fontWeight: FontWeight.bold
+                    ),
+                    
+                  )
+                ]
+              ),
+              ),
+              
+            ],
+          ),
         ),
       )
     );
--- lib/feature/auth/presentation/widgets/auth_field.dart
+++ lib/feature/auth/presentation/widgets/auth_field.dart
@@ -2,17 +2,32 @@ import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
   final String hintText;
+  final TextEditingController controller; // TextEditingController type of variable intialised
+  final bool isobsecureText; // all the fields here are to be initialised inside the constructor
   const AuthField({
     super.key,
     required this.hintText,
+    required this.controller,
+    this.isobsecureText = false,
+    
     });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
       ),
+      validator: (value) {
+        if(value!.isEmpty){
+          return "$hintText is missing!";
+        }
+        else{
+          return null;
+        }
+      },
+      obscureText: isobsecureText,
     );
   }
 }
